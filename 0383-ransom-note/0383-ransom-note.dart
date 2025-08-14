008b092c7c05fcef488f7276d8c68205e92a91e7
hayancode <dudals9696@gmail.com>
2025-08-14 10:40:43 +0900

    Time: 1 ms (100%), Space: 153 MB (11.11%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.dart
@@ -0,0 +1,19 @@
+class Solution {
+  bool canConstruct(String ransomNote, String magazine) {
+    List<int> charCount = List.filled(26, 0);
+  
+    for (int i = 0; i < magazine.length; i++) {  
+        charCount[magazine.codeUnitAt(i) - 'a'.codeUnitAt(0)]++;
+    }
+    
+    for (int i = 0; i < ransomNote.length; i++) {
+        int index = ransomNote.codeUnitAt(i) - 'a'.codeUnitAt(0);
+        if (charCount[index] == 0) {
+        return false;
+        }
+        charCount[index]--;
+    }
+    
+    return true;
+  }
+}
