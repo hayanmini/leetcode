ab686a0c03ed6c1167e8e0e1a24941951a844430
hayancode <dudals9696@gmail.com>
2025-08-25 20:44:57 +0900

    Time: 0 ms (100%), Space: 147.2 MB (90.79%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.dart
@@ -0,0 +1,15 @@
+class Solution {
+  ListNode? reverseList(ListNode? head) {
+    ListNode? prev = null;
+    ListNode? current = head;
+    
+    while (current != null) {
+        ListNode? nextTemp = current.next;  
+        current.next = prev;              
+        prev = current;                    
+        current = nextTemp;                 
+    }
+    
+    return prev;
+  }
+}
