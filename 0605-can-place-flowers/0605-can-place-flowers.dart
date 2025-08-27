8a640ac82d57158461dea193dd8da57850c01d75
hayancode <dudals9696@gmail.com>
2025-08-27 18:59:07 +0900

    Time: 1 ms (91.67%), Space: 148.3 MB (100%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.dart
@@ -0,0 +1,19 @@
+class Solution {
+  bool canPlaceFlowers(List<int> flowerbed, int n) {
+    int count = 0;
+    int length = flowerbed.length;
+
+    for (int i = 0; i < length; i++) {
+        if (flowerbed[i] == 0 &&
+            (i == 0 || flowerbed[i - 1] == 0) &&
+            (i == length - 1 || flowerbed[i + 1] == 0)) {
+        flowerbed[i] = 1; // 꽃을 심음
+        count++;
+        if (count >= n) {
+            return true; 
+        }
+      }
+    }
+    return count >= n;
+    }
+}
